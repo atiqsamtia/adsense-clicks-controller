38a6e67d2721e93c962fb8c2a995ccbed64cd6a5
Atiq Samtia <atiq@falconspk.net>
2013-07-29 17:03:15 +0500

    Create example.php

---
+++ example.php
@@ -0,0 +1,12 @@
+<?php
+include "adsensemain.php";
+?>
+<body onload="checkCookie()">
+
+            
+<center>
+     <div id="blockMe" class="blockMe">
+         Your Ad Code Here <br> <a href="http://falconspk.net"><img src="http://falconspk.net/logo.png"></a>
+
+        </div></center>
+</body>
