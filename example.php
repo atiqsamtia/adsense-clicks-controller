2ae189d564b7e6366d9e0824741effaf538d91e8
Atiq Samtia <atiq@falconspk.net>
2015-08-07 13:24:43 +0500

    changed to support new multi click feature
    it will allow to click maximum 3 clicks. (line # 3)

--- example.php
+++ example.php
@@ -1,11 +1,11 @@
 <?php
 include "mainadsense.php";
 ?>
-<body onload="checkCookie()">
+<body onload="checkCookie(3)">
 
             
 <center>
-     <div id="blockMe" class="blockMe">
+     <div id="blockMe">
          Your Ad Code Here <br> <a href="http://falconspk.net"><img src="http://falconspk.net/logo.png"></a>
 
         </div></center>
