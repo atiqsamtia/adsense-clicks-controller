41ae813c7cc1ce81387a151e4e2fdab32fd731c0
Atiq Samtia <atiq@falconspk.net>
2015-08-07 14:49:30 +0500

    Update example.php

--- example.php
+++ example.php
@@ -1,11 +1,11 @@
 <?php
 include "mainadsense.php";
 ?>
-<body onload="falcons_adclick_load(3)">
+<body onload="falcons_adclick_load('example',3)">
 
             
 <center>
-     <div id="blockMe">
+     <div id="falcons_adclick_ad">
          Your Ad Code Here <br> <a href="http://falconspk.net"><img src="http://falconspk.net/logo.png"></a>
 
         </div></center>
